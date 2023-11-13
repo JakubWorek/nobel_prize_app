3676a71f81423ac734efa0e3acdd0de08d16ddb9
Jakub Worek <worekkuba@gmail.com>
2023-11-13 13:43:28 +0100

    filtering by category added

--- src/pages/PrizeTable.tsx
+++ src/pages/PrizeTable.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import {  Table, TableHead, TableBody, TableRow, TableContainer, TableCell, TableSortLabel, 
-          Box, Paper, Button} from '@mui/material';
+          Box, Paper, Button, FormControl, InputLabel, Input} from '@mui/material';
 
 interface Prize {
   awardYear: string,
@@ -24,6 +24,7 @@ export default function PrizeTable() {
   const [prizes, setPrizes] = useState<Prize[]>([]);
   const [sortCategory, setSortCategory] = useState<boolean>(false);
   const [sortDateAwarded, setSortDateAwarded] = useState<boolean>(false);
+  const [category, setCategory] = useState<string | undefined>(undefined);
 
   const {language, year} = useParams();
   const navigate = useNavigate();
@@ -35,17 +36,25 @@ export default function PrizeTable() {
                       .then(data => data.nobelPrizes)
     };
 
+    const filterByCategory = (data: Prize[], category: string | undefined) => {
+      if (!category) {
+        return data;
+      }
+      return data.filter(prize => prize.category[language as languageType].toLowerCase().includes(category.toLowerCase()));
+    };
+
     fetchData()
       .then( (data: Prize[]) => {
         if (validateLanguage(language) && validateYear(data, year)) {
-          setPrizes(data.filter(prize => prize.awardYear === year));
+          const filteredPrizes = filterByCategory(data.filter(prize => prize.awardYear === year), category);
+          setPrizes(filteredPrizes);
         } else {
           navigate('/');
         }
       })
       .catch(error => console.log(error));
 
-  }, [language, year, navigate])
+  }, [language, year, category, navigate])
 
   const sortByCategory = () => {
     const sortedPrizes = prizes.sort((a, b) => a.category[language as languageType].localeCompare(b.category[language as languageType]))
@@ -59,6 +68,10 @@ export default function PrizeTable() {
     setPrizes(sortDateAwarded ? sortedPrizes.reverse() : sortedPrizes)
   }
 
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCategory(event.target.value);
+  };
+
   const handleBack = () => {
     navigate('/');
   }
@@ -66,7 +79,13 @@ export default function PrizeTable() {
   return (
     <Box sx={{ minWidth: 120, padding: 2, borderRadius: 2, boxShadow: 5, textAlign: "center"}}>
       <h1>Nobel prizes for year {year}</h1>
-      <TableContainer component={Paper}>
+      <Box component="form" sx={{padding: 2}}>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="category">Filter by category</InputLabel>
+          <Input id="category" value={category} onChange={handleFilterChange} />
+        </FormControl>
+      </Box>
+      <TableContainer component={Paper} sx={{ boxShadow: 3 }}>
         <Table>
           <TableHead>
             <TableCell align="left">Year</TableCell>
@@ -95,7 +114,7 @@ export default function PrizeTable() {
         </Table>
       </TableContainer>
       <Box component="div" sx={{marginTop: 2}}>
-        <Button variant="contained" onClick={handleBack}>Back</Button>
+        <Button variant="contained" onClick={handleBack} sx={{ boxShadow: 5 }}>Back</Button>
       </Box>
     </Box>
   )
